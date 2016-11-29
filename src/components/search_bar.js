ece68b4f27a092d0e3e1ae5b9b4b020bbb299e0c
Samantha Spensley <spenslsm@gmail.com>
2016-11-29 09:59:26 -0700

    reworked search bar to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />  // react.createElement('input')
-};
+// functional component
+// const SearchBar = () => {
+//   return <input />  // react.createElement('input')
+// };
+
+// class component allows for internal record keeping
+// Define a new class and give it access to all of the functionality that React.Component class has
+// all classes must have render function that returns JSX
+class SearchBar extends React.Component {
+  render() {
+    return <input />;
+  }
+}
 
 // any file that imports SearchBar has access to SearchBar component
 export default SearchBar;
