1740054e59b37325878afd4f15e8adf6411013b9
Samantha Spensley <spenslsm@gmail.com>
2016-11-29 08:30:49 -0700

    basic react setup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
+// writing Component/views are snippets of code that produce html
+// nest Components to make complex apps relatively simple
+
+// react create and manage components
+// reactDOM interacts with DOM
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create new component that will produce some html
+// return JSX (subset of JS)
+const App = () => {
+  return <div>Hi!</div>
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take component's generated HTML and put in the DOM
+// (App) = class of component; (<App />) = instance of component
+// document.querySelector('container') targets DOM node 'container' from index.html
+ReactDOM.render(<App />, document.querySelector('.container'));
