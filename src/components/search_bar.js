a1a2775c5b6cbd343274f5726523154bb56c983c
Samantha Spensley <spenslsm@gmail.com>
2016-11-29 10:26:50 -0700

    set state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,8 +10,22 @@ import React, { Component } from 'react';
 // Define a new class and give it access to all of the functionality that React.Component class has
 // all classes must have render method that returns JSX
 class SearchBar extends Component {
+  // initialize state; constructor function called automatically whenever new instance of component is called
+  constructor(props) {
+    // super refers to parent method
+    super(props);
+
+    this.state = { term: '' };
+  }
+
   render() {
-    return <input onChange={event => console.log(event.target.value)} />;
+    // always manipulate state with this.setState!!
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+        Value of the input: {this.state.term}
+      </div>
+    )
   }
 }
 
